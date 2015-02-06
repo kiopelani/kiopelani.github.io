fb1119eb31418245344b9b79dfda479f0c00bb26
Kiope Gyzen <kiopelani@gmail.com>
2015-02-06 09:58:30 -0800

    refactored js file

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,47 +1,37 @@
-$(document).ready(function(){
-  console.log("I'm readddyyyy!");
-  $('#img0').delay(300).fadeTo(300, .5);
-  $('#img0').fadeTo(300, 1, function(){
-    $('#img1').fadeTo(300, .5);
-    $('#img1').fadeTo(300, 1, function(){
-      $('#img2').fadeTo(300, .5);
-      $('#img2').fadeTo(300, 1, function(){
-        $('#img3').fadeTo(300, .5);
-        $('#img3').fadeTo(300, 1, function(){
-          $('#img4').fadeTo(300, .5);
-          $('#img4').fadeTo(300, 1, function(){
-            $('#img5').fadeTo(300, .5);
-            $('#img5').fadeTo(300, 1, function(){
-              $('#img6').fadeTo(300, .5);
-              $('#img6').fadeTo(300, 1, function(){
-                $('#img7').fadeTo(300, .5);
-                $('#img7').fadeTo(300, 1, function(){
-                  $('#img8').fadeTo(300, .5);
-                  $('#img8').fadeTo(300, 1, function(){
-                    $('#img9').fadeTo(300, .5);
-                    $('#img9').fadeTo(300, 1, function(){
-                      $('#title-name').fadeTo(1000, 1, function(){
-                        $('#title-sub').fadeTo(1000, 1, function(){
-                          $('#down-arrow').slideDown(500);
-                        });
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
+function loadImages(){
+  var $kids = $('#landing').children();
+  $.each($kids, function(key, value){
+    timePassed += 300;
+    $(value).delay(timePassed).fadeTo(300, .5).fadeTo(300, 1);
+  })
+}
 
-    })
-  });
-$('.skills-container').hover(function(){
-  $('.skills-container').fadeTo(500, .7);},
-  function(){
+function loadTitles(){
+  var $titles = $('#title').children();
+  $.each($titles, function(key, value){
+    timePassed += 400;
+    $(value).delay(timePassed).fadeTo(1000, 1);
+  })
+}
+
+function loadArrow(){
+  $('#down-arrow').delay(timePassed + 300).slideDown(500);
+}
+
+function initSkillsContainerHover(){
+  $('.skills-container').hover(function(){
+    $('.skills-container').fadeTo(500, .7);},
+    function(){
     $('.skills-container').fadeTo(500, 1);
-  }
+    }
   )
+}
 
+$(document).ready(function(){
+  timePassed = 0;
+  loadImages();
+  loadTitles();
+  loadArrow();
 
+  initSkillsContainerHover();
 })
